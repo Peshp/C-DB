9ed46d536390766c18f0ee6b7dd345c7f7552efc
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-01-22 15:26:35 +0200

    Add files via upload

---
+++ Table Relations/01. One-To-One Relationship.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE Relationship
+
+USE [Relationship]
+
+CREATE TABLE Passports(
+	[PassportID] INT PRIMARY KEY IDENTITY (101, 1),
+	[PassportNumber] VARCHAR(50) NOT NULL
+)
+INSERT INTO [Passports] VALUES
+	('N34FG21B'),
+	('K65LO4R7'),
+	('ZE657QP2')
+
+CREATE TABLE Persons(
+	[PersonID] INT PRIMARY KEY IDENTITY,
+	[FirstName] VARCHAR(20) NOT NULL,
+	[Salary] DECIMAL(7, 2) NOT NULL,
+	[PassportID] INT FOREIGN KEY REFERENCES[Passports](PassportID)
+)
+INSERT INTO [Persons] VALUES
+	('Roberto', 43300.00, 102), 
+	('Tom', 56100.00, 103),
+	('Yana', 60200.00, 101)
+
