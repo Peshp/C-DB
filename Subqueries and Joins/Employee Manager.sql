968b10f7291100b0e04c230b84938585dd4d1930
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-02-05 17:59:07 +0200

    Add files via upload

---
+++ Subqueries and Joins/Employee Manager.sql
@@ -0,0 +1,7 @@
+SELECT e.EmployeeID, e.FirstName,
+		m.EmployeeID AS ManagerID,
+		m.FirstName AS ManagerName
+FROM Employees AS e
+JOIN Employees AS m ON m.EmployeeID = e.ManagerID
+	AND e.ManagerID IN (3, 7)
+ORDER BY e.EmployeeID
