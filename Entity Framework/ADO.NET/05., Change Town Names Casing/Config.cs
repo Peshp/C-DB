330ffac22e99fdec1e5b34cd3537becaf2fb1d84
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-02-19 15:26:43 +0200

    Add files via upload

---
+++ Entity Framework/ADO.NET/05., Change Town Names Casing/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.__Change_Town_Names_Casing
+{
+    public class Config
+    {
+        public const string Sqlconnection =
+            @"Server=DESKTOP-E8HCIGG\SQLEXPRESS;Database=MinionsDB;Integrated Security=True;";
+    }
+}
---
+++ Entity Framework/ADO.NET/05., Change Town Names Casing/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace _05.__Change_Town_Names_Casing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using SqlConnection sql = new SqlConnection(Config.Sqlconnection);
+            sql.Open();
+            UpdateTownName(Console.ReadLine(), sql);
+            sql.Close();                   
+        }
+
+        public static void UpdateTownName(string v, SqlConnection sql)
+        {
+            string updateTownsQuery = @"UPDATE Towns
+                                        SET Name = UPPER(Name)
+                                        WHERE CountryCode = (
+                                        SELECT c.Id FROM Countries AS c 
+                                        WHERE c.Name = @countryName)";
+            SqlCommand updateTownsCmd = new SqlCommand(updateTownsQuery, sql);
+            updateTownsCmd.Parameters.AddWithValue("@countryName", v);
+
+            string townNamesQuery = @" SELECT t.Name 
+                                       FROM Towns as t
+                                       JOIN Countries AS c ON c.Id = t.CountryCode
+                                       WHERE c.Name = @countryName";
+            SqlCommand townNamesCmd = new SqlCommand(townNamesQuery, sql);
+            townNamesCmd.Parameters.AddWithValue("@countryName", v);
+            SqlDataReader reader = townNamesCmd.ExecuteReader();
+
+            if(!reader.HasRows)
+                Console.WriteLine("No town names were affected.");
+            else
+            {
+                var affectedTowns = new List<string>();
+                while (reader.Read())
+                    affectedTowns.Add(reader["Name"].ToString());
+
+                Console.WriteLine($"{affectedTowns.Count} town names were affected.");
+                Console.WriteLine($"[{string.Join(", ", affectedTowns)}]");
+            }
+        }
+    }
+}
