19c633574a7b1733b0295e36b9d832783d9dc4da
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-02-19 15:27:09 +0200

    Add files via upload

---
+++ Entity Framework/ADO.NET/07. Print All Minion Names/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07._Print_All_Minion_Names
+{
+    public class Config
+    {
+        public const string Sqlconnection =
+            @"Server=DESKTOP-E8HCIGG\SQLEXPRESS;Database=MinionsDB;Integrated Security=True;";
+    }
+}
---
+++ Entity Framework/ADO.NET/07. Print All Minion Names/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace _07._Print_All_Minion_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using SqlConnection sql = new SqlConnection(Config.Sqlconnection);
+
+            sql.Open();
+            Console.WriteLine(PrintMinionNames(sql)); 
+            sql.Close();
+        }
+
+        public static string PrintMinionNames(SqlConnection sql)
+        {
+            string minionNamesQuery = @"SELECT Name FROM Minions";
+            SqlCommand minionNamesCmd = new SqlCommand(minionNamesQuery, sql);
+            SqlDataReader reader = minionNamesCmd.ExecuteReader();
+
+            var queue = new Queue<string>();
+            var stack = new Stack<string>();
+
+            while (reader.Read())
+            {
+                queue.Enqueue(reader["Name"].ToString());
+                stack.Push(reader["Name"].ToString());
+            }
+
+            return PrintName(queue, stack);
+        }
+
+        private static string PrintName(Queue<string> queue, Stack<string> stack)
+        {
+            StringBuilder sb = new StringBuilder();
+            while(queue.Count > 6 && stack.Count > 5)
+            {
+                sb.AppendLine(queue.Dequeue());
+                sb.AppendLine(stack.Pop());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
