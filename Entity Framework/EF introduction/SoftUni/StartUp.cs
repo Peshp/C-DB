bff58ad92a6dc18881b0d9a89bf97cb3a2f82751
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-02-23 15:49:08 +0200

    Add files via upload

---
+++ Entity Framework/EF introduction/SoftUni/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using SoftUni.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(GetEmployeesFullInformation(new SoftUniContext()));
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employees = context.Employees.ToList();
+            foreach (var e in employees.OrderBy(x => x.EmployeeId))
+            {
+                sb.AppendLine($"{e.FirstName} {e.MiddleName} {e.LastName} {e.JobTitle}" +
+                    $"{e.Salary:f2}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
