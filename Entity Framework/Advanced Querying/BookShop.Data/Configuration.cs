0bfd65936f1e3a7a0ebd88d45062079ea1f19ab1
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2023-03-28 11:24:29 +0300

    Add files via upload

---
+++ Entity Framework/Advanced Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => "Server=DESKTOP-E8HCIGG\\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
